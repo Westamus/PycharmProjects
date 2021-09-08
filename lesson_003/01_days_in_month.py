ac66c26e5554ef8d2497769e0ac56f812e0c7177
Westamus <westamus@gmail.com>
2021-09-08 16:57:02 +0500

    Выполнил 01 задания 3 урока

--- lesson_003/01_days_in_month.py
+++ lesson_003/01_days_in_month.py
@@ -8,7 +8,23 @@
 
 # Номер месяца получать от пользователя следующим образом
 user_input = input("Введите, пожалуйста, номер месяца: ")
+user_input = int(user_input)
 month = int(user_input)
 print('Вы ввели', month)
+month_days_31 = [1, 3, 5, 7, 8, 10, 12]
+month_days_30 = [4, 6, 9, 11]
 
-# TODO здесь ваш код
+for days in month_days_31:
+    if days == user_input:
+        print('Дней в этом месяце -', 31)
+        break
+
+for days in month_days_30:
+    if days == user_input:
+        print('Дней в этом месяце -', 30)
+        break
+
+if user_input == 2:
+    print('Дней в этом месяце -', 28)
+
+
