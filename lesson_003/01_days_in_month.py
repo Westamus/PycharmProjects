32126f9dff5add6c106da4989d164a736d6a55ee
Westamus <westamus@gmail.com>
2021-09-08 17:32:27 +0500

    Добавил решение без for

--- lesson_003/01_days_in_month.py
+++ lesson_003/01_days_in_month.py
@@ -27,4 +27,33 @@ for days in month_days_30:
 if user_input == 2:
     print('Дней в этом месяце -', 28)
 
+# Решение без for
+
+months_day_count = {'1':  31,
+                    '2':  28,
+                    '3':  31,
+                    '4':  30,
+                    '5':  31,
+                    '6':  30,
+                    '7':  31,
+                    '8':  31,
+                    '9':  30,
+                    '10': 31,
+                    '11': 30,
+                    '12': 31,
+                    }
+
+
+user_input = input("Введите, пожалуйста, номер месяца: ")
+if user_input.isdigit():
+    month = int(user_input)
+
+    if 1 <= month <= 12:
+        day_count = months_day_count[user_input]
+        print('Вы ввели', month)
+        print('Кол-во дней в месяце:', day_count)
+    else:
+        print('Месяца с таким номер не существует.')
+else:
+    print('Необходимо ввести число.')
 
