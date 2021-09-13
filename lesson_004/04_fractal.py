b1727f8af9180f78351c94be785da52f6fe404b4
Westamus <westamus@gmail.com>
2021-09-13 16:52:59 +0500

    Выполнил 04 задание 4 урока

--- lesson_004/04_fractal.py
+++ lesson_004/04_fractal.py
@@ -2,6 +2,7 @@
 
 import simple_draw as sd
 
+sd.resolution = (1200, 700)
 # 1) Написать функцию draw_branches, которая должна рисовать две ветви дерева из начальной точки
 # Функция должна принимать параметры:
 # - точка начала рисования,
@@ -16,26 +17,50 @@ import simple_draw as sd
 #   и параметром "длинна ветвей" в 0.75 меньшей чем длина только что нарисованной ветви
 
 # 3) первоначальный вызов:
-# root_point = get_point(300, 30)
-# draw_bunches(start_point=root_point, angle=90, length=100)
-
 # Пригодятся функции
 # sd.get_point()
 # sd.get_vector()
 # Возможный результат решения см lesson_004/results/exercise_04_fractal_01.jpg
-
 # можно поиграть -шрифтами- цветами и углами отклонения
 
-# TODO здесь ваш код
 
 # 4) Усложненное задание (делать по желанию)
-# - сделать рандомное отклонение угла ветвей в пределах 40% от 30-ти градусов
-# - сделать рандомное отклонение длины ветвей в пределах 20% от коэффициента 0.75
+# - сделать рандомное отклонение угла ветвей в пределах 40% от 30-ти градусов (28, 42)
+# - сделать рандомное отклонение длины ветвей в пределах 20% от коэффициента 0.75 (0.55, 0.95)
 # Возможный результат решения см lesson_004/results/exercise_04_fractal_02.jpg
 
 # Пригодятся функции
 # sd.random_number()
 
+root_point = sd.get_point(600, 0)
+
+
+def draw_branches(point, angle, length, delta):
+    if length < 10:
+        return
+    v1 = sd.get_vector(start_point=point, angle=angle, length=length)
+    v1.draw()
+    next_point = v1.end_point
+    rand_angle = int(sd.random_number(0, 1))
+    if rand_angle == 1:
+        delta += sd.random_number(0, 12)
+    else:
+        delta -= sd.random_number(0, 12)
+
+    rand_length = int(sd.random_number(0, 1))
+
+    coefficient_length = .75
+    if rand_length == 1:
+        coefficient_length += sd.random_number(1, 20)/100
+    else:
+        coefficient_length -= sd.random_number(1, 20)/100
+
+    next_angle_right = angle - delta
+    next_angle_left = angle + delta
+    next_length = length * coefficient_length
+    draw_branches(point=next_point, angle=next_angle_right, length=next_length, delta=delta)
+    draw_branches(point=next_point, angle=next_angle_left, length=next_length, delta=delta)
+
+draw_branches(point=root_point, angle=90, length=100, delta=30)
+
 sd.pause()
-
-
